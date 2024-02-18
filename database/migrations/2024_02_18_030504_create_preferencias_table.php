a28d578f18d3c9bf1ddd663638a11435ae40c7a0
Andresfonse <cristiandeavila747@gmail.com>
2024-02-17 23:57:17 -0500

    PWA SUPPORT CHAT

---
+++ database/migrations/2024_02_18_030504_create_preferencias_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePreferenciasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('preferencias', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('foro');
+            $table->string('comments');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('preferencias');
+    }
+}
---
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>You are currently not connected to any networks.</h1>
+
+    40404404040 no conexion 
+
+@endsection
